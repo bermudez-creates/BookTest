c9aeefe1622ad806f48f2985c43ff6e1d3b2cd2d
Alejandro Bermudez <82286169+bermudez-creates@users.noreply.github.com>
2023-05-23 14:22:47 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import express, { query }  from "express";
+import mysql from "mysql";
+import cors from "cors";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = express()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'S3rHum1ld3*',
+    database: 'test',
+})
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => { 
+    res.json("This is a test")
+}) 
+
+app.get('/books', (req, res) => {
+    const q = "SELECT * FROM books"
+    db.query(q, (err,data) => { 
+        if(err) return res.json(err)
+        return res.json(data)
+ 
+    })
+    
+})
+
+app.post("/books", (req, res) => {
+    const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)";
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.price,
+        req.body.cover
+        ];
+
+    db.query(q, [values], (err,data) => {
+        if(err) return res.json(err)
+        return res.json("Success")
+    });
+});
+
+app.delete("/books/:id", (req, res) => {
+    const bookId = req.params.id;
+    const q = "DELETE FROM books WHERE  id = ?" ;
+
+    db.query(q, [bookId], (err, data) => {
+        if (err) return res.json(err);
+        return res.json("Deleted!");
+    }); 
+}); 
+
+app.put("/books/:id", (req, res) => {
+    const bookId = req.params.id;
+    const q = "UPDATE books SET `title` = ?, `desc` = ?, `price` = ?, `cover` = ? WHERE id = ?" ;
+
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.price,
+        req.body.cover
+        ];
+
+    db.query(q, [...values,bookId], (err, data) => {
+        if (err) return res.json(err);
+        return res.json("Updated successfully!");
+    }); 
+}); 
+
+app.listen(8808, () => {
+    console.log("Hello world!");
+})
